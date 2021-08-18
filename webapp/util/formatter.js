f76f542e7844a9145958bc331fa7e9855a82e52a
Guilherme Souza <guilherme.souza@numenit.com>
2021-08-18 19:38:15 +0000

    first commit

---
+++ webapp/util/formatter.js
@@ -0,0 +1,43 @@
+sap.ui.define([], function() {
+    "use strict";
+
+    return{
+        toCurrency: function(sValue) {
+            return sValue !== undefined ? Number(sValue).toFixed(2) : undefined;
+        }
+    }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
